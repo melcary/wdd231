72c8958921ce88d57ddce8b3fdf6283c77903731
melcary <melcary13@gmail.com>
2026-05-06 11:28:38 -0500

    Site navigation

--- code-along-header/scripts/navigation.js
+++ code-along-header/scripts/navigation.js
@@ -1,5 +1,8 @@
 const navbuttom = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
 
 navbuttom.addEventListener('click', () => {
     navbuttom.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
+
